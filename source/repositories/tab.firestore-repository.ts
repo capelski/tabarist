069a1a423084362248c0efd0eea3da3d3b74618f
Carles Capellas <capellas.carles@gmail.com>
2025-01-19 17:23:44 +0100

    Change firestore path for tabs

--- source/repositories/tab.firestore-repository.ts
+++ source/repositories/tab.firestore-repository.ts
@@ -2,17 +2,25 @@ import { User } from '../firebase';
 import { deleteDocument, getCollectionDocuments, getDocument, setDocument } from '../firestore';
 import { Tab } from '../types';
 
+const getUserTabsPath = (userId: User['uid']) => {
+  return ['users', userId, 'tabs'];
+};
+
+const getTabPath = (userId: User['uid'], tabId: string) => {
+  return [...getUserTabsPath(userId), tabId];
+};
+
 export const tabFirestoreRepository = {
   getById: (tabId: string, userId: User['uid']) => {
-    return getDocument(['tabs', userId, 'private', tabId]) as Promise<Tab>;
+    return getDocument(getTabPath(userId, tabId)) as Promise<Tab>;
   },
   getMany: (userId: User['uid'], limit = 10) => {
-    return getCollectionDocuments(['tabs', userId, 'private'], limit) as Promise<Tab[]>;
+    return getCollectionDocuments(getUserTabsPath(userId), limit) as Promise<Tab[]>;
   },
   remove: (tabId: string, userId: User['uid']) => {
-    return deleteDocument(['tabs', userId, 'private', tabId]);
+    return deleteDocument(getTabPath(userId, tabId));
   },
   set: (tab: Tab, userId: User['uid']) => {
-    return setDocument(['tabs', userId, 'private', tab.id], tab);
+    return setDocument(getTabPath(userId, tab.id), tab);
   },
 };
