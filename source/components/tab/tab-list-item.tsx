eca02c956f0cafcdba0e9b96f0748584cca7dcf0
Carles Capellas <capellas.carles@gmail.com>
2025-05-08 23:29:12 +0200

    Avoid redundant network query when navigating to list items

--- source/components/tab/tab-list-item.tsx
+++ source/components/tab/tab-list-item.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router';
 import { removeSymbol } from '../../constants';
 import { getTabRelativeUrl } from '../../operations';
-import { StarredTab } from '../../types';
+import { ActionType, StateProvider } from '../../state';
+import { StarredTab, Tab } from '../../types';
 
 export type TabListItemProps = {
   allowRemoving: boolean;
   remove: () => void;
-  tab: StarredTab;
+  tab: StarredTab | Tab;
 };
 
 export const TabListItem: React.FC<TabListItemProps> = (props) => {
+  const { dispatch } = useContext(StateProvider);
+
   return (
     <div
       key={props.tab.id}
       style={{ display: 'flex', justifyContent: 'space-between', marginBottom: 8 }}
     >
-      <NavLink className="nav-link" style={{ flexGrow: 1 }} to={getTabRelativeUrl(props.tab.id)}>
+      <NavLink
+        className="nav-link"
+        style={{ flexGrow: 1 }}
+        onClick={() => {
+          if ('bars' in props.tab) {
+            // Set the tab here to prevent a redundant network query
+            dispatch({ type: ActionType.setTab, tab: props.tab });
+          }
+        }}
+        to={getTabRelativeUrl(props.tab.id)}
+      >
         {props.tab.title}
       </NavLink>
       {props.allowRemoving && (
