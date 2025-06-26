e1dd9306c2db5a0d11e10e2968ae711da8f15af7
Carles Capellas <capellas.carles@gmail.com>
2025-06-26 23:41:59 +0200

    Fix server side rendering issue

--- source/classes/beat-engine.ts
+++ source/classes/beat-engine.ts
@@ -3,12 +3,14 @@ import { BeatEngineCore } from './beat-engine-core';
 
 const clickAudio = typeof Audio !== 'undefined' ? new Audio(clickSound) : undefined;
 
+const context = typeof window === 'undefined' ? global : window;
+
 export class BeatEngine extends BeatEngineCore {
   protected youtubeDelayTimeout: number | undefined;
 
   constructor() {
     super({
-      clearTimeout: window.clearTimeout.bind(window),
+      clearTimeout: context.clearTimeout.bind(context),
       getLastDelay: Date.now.bind(Date),
       getLastRendered: Date.now.bind(Date),
       initializeYoutubePlayer: (videoId: string, start?: number) => {
@@ -41,12 +43,12 @@ export class BeatEngine extends BeatEngineCore {
           });
         });
       },
-      scheduleBeat: window.setTimeout.bind(window),
-      scheduleCountdown: window.setTimeout.bind(window),
-      scheduleTrackStart: window.setTimeout.bind(window),
+      scheduleBeat: context.setTimeout.bind(context),
+      scheduleCountdown: context.setTimeout.bind(context),
+      scheduleTrackStart: context.setTimeout.bind(context),
       startYoutubeTrack: (start) => {
         return new Promise<void>((resolve) => {
-          this.youtubeDelayTimeout = window.setTimeout(resolve, start);
+          this.youtubeDelayTimeout = setTimeout(resolve, start) as unknown as number;
           this.youtubePlayer!.playVideo();
         });
       },
